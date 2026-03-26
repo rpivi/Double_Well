a,b where not in the E and Cv calculation, now fixed

source/observable.py
@@ -14,20 +14,17 @@ def blocking_error(data, block_size):
 
 @jax.jit
 def V(x, a=1.0, b=1.0):
-    if x.shape[0] == 1:
-        return a * (x[0]**2 - b)**2
-    else:
-        return a * (x[0]**2 - b)**2 + 0.5 * jnp.sum(x[1:]**2)
+    return a * (x[0]**2 - b)**2 + 0.5 * jnp.sum(x[1:]**2)
 
-def mean_energy(trajectory, block_size):
-    energies = jax.vmap(V)(trajectory)
+def mean_energy(trajectory, block_size, a=1.0, b=1.0):
+    energies = jax.vmap(lambda x: V(x, a, b))(trajectory)
     mean_energy = jnp.mean(energies)
     # error on mean energy using blocking
     E_mean_err = blocking_error(energies, block_size)
     return mean_energy, E_mean_err
 
-def heat_capacity(trajectory, T, block_size):
-    energies = jax.vmap(V)(trajectory)
+def heat_capacity(trajectory, T, block_size, a=1.0, b=1.0):
+    energies = jax.vmap(lambda x: V(x, a, b))(trajectory)
     mean_E = jnp.mean(energies)
     mean_E2 = jnp.mean(energies**2)
     Cv = (mean_E2 - mean_E**2) / (T**2)
@@ -56,11 +53,11 @@ def integrated_autocorrelation_time(x, c=5.0):
         tau += corr[t]
     return tau
 
-def append_observables(results, trajectories, D, T, trajectory, acceptance_rate):
+def append_observables(results, trajectories, D, T, trajectory, acceptance_rate, a=1.0, b=1.0):
     tau_x= integrated_autocorrelation_time(trajectory[:,0])
     block_size = int(jnp.maximum(10, 5*tau_x))
-    E_mean, E_mean_err = mean_energy(trajectory, block_size)
-    Cv, Cv_err = heat_capacity(trajectory, T, block_size)
+    E_mean, E_mean_err = mean_energy(trajectory, block_size, a, b)
+    Cv, Cv_err = heat_capacity(trajectory, T, block_size, a,b)
 
     results[D]["T"].append(T)
     